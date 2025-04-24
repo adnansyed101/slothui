4862c102960bf05c953f1124f44a721fd6701f65
Adnan <sirgeant10@gmail.com>
2025-04-24 20:57:22 +0600

    Add testimonials using js.

--- js/section.js
+++ js/section.js
@@ -1,8 +1,3 @@
-const companiesContainer = document.getElementById("logo-container");
-const featuresContainer = document.getElementById("features-container");
-const achievementsContainer = document.getElementById("achievements-container");
-const forWhomContainer = document.getElementById("for-whom-container");
-
 const companyLogos = [
   { src: "delta.png", alt: "Delta" },
   { src: "boeing.png", alt: "Boeing" },
@@ -74,7 +69,94 @@ const achievements = [
   { title: "1.82M", subtitle: "AI LLM Scale" },
 ];
 
+const testimonials = [
+  {
+    stars: "5.png",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur ad-zzz-ing elit. Proin vel urna at metus tempor mattis, or not, whatever.",
+    avatar: "avatar-1.png",
+    name: "X_AE_A-13",
+    position: "Product Designer, slothUI",
+  },
+  {
+    stars: "5.png",
+    comment:
+      "Nisi porta lorem mollis aliquam ut porttitor leo. Sed odio morbi quis commodo. Arcu non odio euismod lacinia at quis. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae.",
+    avatar: "avatar-4.png",
+    name: "Oarack Babama",
+    position: "Former President of US",
+  },
+  {
+    stars: "2.5.png",
+    comment:
+      "Sagittis eu volutpat odio facilisis mauris sit amet massa. Urna et pharetra pharetra massa.",
+    avatar: "avatar-7.png",
+    name: "Boe Jiden",
+    position: "Former President Of Canada",
+  },
+  {
+    stars: "4.5.png",
+    comment:
+      "Posuere urna nec tincidunt praesent semper feugiat nibh sed. Velit dignissim sodales ut eu.",
+    avatar: "avatar-2.png",
+    name: "Azunyan U. Wu",
+    position: "CEO, nextlife.ai",
+  },
+  {
+    stars: "4.5.png",
+    comment:
+      "Accumsan tortor posuere ac ut consequat. Quis varius quam quisque id diam vel. Commodo odio aenean sed adipiscing diam donec adipiscing. Vel pharetra vel turpis nunc eget.",
+    avatar: "avatar-5.png",
+    name: "Saylor Twift",
+    position: "Famous Singer",
+  },
+  {
+    stars: "5.png",
+    comment:
+      "Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Nec nam aliquam sem et tortor consequat id porta nibh. Viverra suspendisse. Lacus sed viverra tellus in hac habitasse platea dictumst. Elit at imperdiet dui accumsan.",
+    avatar: "avatar-8.png",
+    name: "Vermillion D. Gray",
+    position: "CEO, hacklife.ai",
+  },
+  {
+    stars: "4.png",
+    comment:
+      "Sit amet est placerat in. Blandit cursus risus at ultrices mi tempus imperdiet. Volutpat commodo sed egestas egestas fringilla phasellus. Tincidunt eget nullam non nisi.",
+    avatar: "avatar-3.png",
+    name: "Mechatronics Yi",
+    position: "CTO, fin4win.ai",
+  },
+
+  {
+    stars: "3.5.png",
+    comment:
+      "Venenatis cras sed felis eget velit aliquet. Velit scelerisque in dictum non consectetur. Felis eget velit aliquet sagittis. Ultricies integer quis auctor elit sed vulputate mi.",
+    avatar: "avatar-6.png",
+    name: "Asuna Yuuki",
+    position: "Virtual Swordsman",
+  },
+
+  {
+    stars: "4.png",
+    comment:
+      "Viverra accumsan in nisl nisi scelerisque eu. Vitae congue eu consequat ac felis donec.",
+    avatar: "avatar-9.png",
+    name: "Zuckman Wu",
+    position: "CEO, tech4life.ai",
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
+  const companiesContainer = document.getElementById("logo-container");
+  const featuresContainer = document.getElementById("features-container");
+  const achievementsContainer = document.getElementById(
+    "achievements-container"
+  );
+  const forWhomContainer = document.getElementById("for-whom-container");
+  const testimonialsContainer = document.getElementById(
+    "testimonials-container"
+  );
+
   // Company Logos Section
   companyLogos.forEach((logo) => {
     companiesContainer.innerHTML += `
@@ -98,6 +180,7 @@ document.addEventListener("DOMContentLoaded", () => {
 `;
   });
 
+  // Achievements
   achievements.forEach((achievement) => {
     achievementsContainer.innerHTML += `
     <div>
@@ -107,6 +190,7 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
   });
 
+  // For whom section cards
   forWhomText.forEach((text) => {
     forWhomContainer.innerHTML += `
     <div class="card">
@@ -116,6 +200,27 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${text.subtitle}</p>
         </div>
     </div>
+    `;
+  });
+
+  testimonials.forEach((testimonial) => {
+    testimonialsContainer.innerHTML += `
+     <div class="testimonial">
+        <img src="./assets/testimonials/stars/${testimonial.stars}" alt="Stars" />
+        <p>
+          ${testimonial.comment}
+        </p>
+        <div class="person">
+          <img
+            src="./assets/testimonials/avatar/${testimonial.avatar}"
+            alt="${testimonial.name}"
+          />
+          <div>
+            <h6>${testimonial.name}</h6>
+            <p>${testimonial.position}</p>
+          </div>
+        </div>
+      </div>
     `;
   });
 });
